7c93d370000625d42a10b483a4013ed5b98274ea
beaudeyoung <beau@beaudeyoung.com>
2020-03-10 13:53:23 -0400

    Add project files.

---
+++ MVCDemo/Controllers/MembershipController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    [Authorize]
+    public class MembershipController : Controller
+    {
+        private MemberDBContext db = new MemberDBContext();
+
+        // GET: Membership
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View(db.Members.ToList());
+        }
+
+        // GET: Membership/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Membership membership = db.Members.Find(id);
+            if (membership == null)
+            {
+                return HttpNotFound();
+            }
+            return View(membership);
+        }
+
+        // GET: Membership/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Membership/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,fname,lname,gender,age")] Membership membership)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Members.Add(membership);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(membership);
+        }
+
+        // GET: Membership/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Membership membership = db.Members.Find(id);
+            if (membership == null)
+            {
+                return HttpNotFound();
+            }
+            return View(membership);
+        }
+
+        // POST: Membership/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,fname,lname,gender,age")] Membership membership)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(membership).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(membership);
+        }
+
+        // GET: Membership/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Membership membership = db.Members.Find(id);
+            if (membership == null)
+            {
+                return HttpNotFound();
+            }
+            return View(membership);
+        }
+
+        // POST: Membership/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Membership membership = db.Members.Find(id);
+            db.Members.Remove(membership);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
